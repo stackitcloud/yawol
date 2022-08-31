c36ae01af244b276dce888fba21b188e8e8540f5
Maximilian Geberl <maximilian.geberl@stackit.de>
2022-08-31 14:11:45 +0200

    add comments my there are spaces in the yaml keys

--- image/keepalivedstats-script.sh
+++ image/keepalivedstats-script.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+# This script creates/updates /tmp/keepalived.stats for yawollet every 20 seconds
 
 while true
 do
+  # Send USR2 signal to keepalived for the creation/update of /tmp/keepalived.stats
   kill -USR2 $(cat /var/run/keepalived.pid)
+  # Change file permissions for /tmp/keepalived.stats to 644 so yawollet can read it
   chmod 644 /tmp/keepalived.stats
   sleep 20s
 done
