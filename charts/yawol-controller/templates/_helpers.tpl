aa03b3e6830818e439a1ea7f090356f8cfc1a043
Felix Breuer <fbreuer@pm.me>
2022-09-21 10:14:01 +0200

    clean branch
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- charts/yawol-controller/templates/_helpers.tpl
+++
@@ -1,3 +0,0 @@
-{{- define "deploymentversion" -}}
-apps/v1
-{{- end -}}
--- tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-// +build tools
-
-package main
-
-import (
-	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
-)
