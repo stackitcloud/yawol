9288bffef1e85269dd629af429fe7c9705818a9f
Maximilian Geberl <maximilian.geberl@stackit.de>
2022-11-10 14:32:01 +0100

    add logging for yawollet

--- image/yawollet.sh
+++ image/yawollet.sh
@@ -8,7 +8,9 @@ supervisor="supervise-daemon"
 command="/usr/local/bin/yawollet"
 command_args="$YAWOLLET_ARGS"
 command_user="yawol"
+output_log="/var/log/yawol/yawollet.log"
+error_log="/var/log/yawol/yawollet.log"
 
 depend() {
-	after net 
+	after net
 }
