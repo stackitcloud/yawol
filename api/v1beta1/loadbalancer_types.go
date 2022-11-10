a0817dc15d9973d1047cc32337ffdb271084be7e
Maximilian Geberl <maximilian.geberl@stackit.de>
2022-11-10 14:35:19 +0100

    add logforward fields in LoadBalancer Object

--- api/v1beta1/loadbalancer_types.go
+++ api/v1beta1/loadbalancer_types.go
@@ -100,6 +100,19 @@ type LoadBalancerOptions struct {
 	// If empty it is enabled for all ports. Only has an affect if TCPProxyProtocol is enabled.
 	// +optional
 	TCPProxyProtocolPortsFilter []int32 `json:"tcpProxyProtocolPortFilter,omitempty"`
+	// LogForward enables log forward to a loki instance
+	// +optional
+	LogForward LoadBalancerLogForward `json:"logForward,omitempty"`
+}
+
+// LoadBalancerLogForward enables log forward to a loki instance.
+type LoadBalancerLogForward struct {
+	// Enabled defines if log forward is enabled
+	// +optional
+	Enabled bool `json:"enabled,omitempty"`
+	// LokiUrl defines the loki push url (Example: http://example.com:3100/loki/api/v1/push).
+	// +optional
+	LokiUrl string `json:"lokiUrl,omitempty"`
 }
 
 // LoadBalancerDebugSettings defines debug settings for the LoadBalancer
