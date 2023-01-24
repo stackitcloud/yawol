1791a5157711df7cbf017ec94a15e29d88b0f46b
Felix Breuer <fbreuer@pm.me>
2023-01-24 15:59:51 +0100

    retrieves kubernetes version on server

---
+++ internal/helper/kubernetes/version.go
@@ -0,0 +1,33 @@
+package kubernetes
+
+import (
+	"strconv"
+
+	discovery "k8s.io/client-go/discovery"
+	"k8s.io/client-go/rest"
+)
+
+type Version struct {
+	Major int
+	Minor int
+}
+
+func GetVersion(cfg *rest.Config) (*Version, error) {
+	client := discovery.NewDiscoveryClientForConfigOrDie(cfg)
+	version, err := client.ServerVersion()
+	if err != nil {
+		return nil, err
+	}
+
+	major, err := strconv.Atoi(version.Major)
+	if err != nil {
+		return nil, err
+	}
+
+	minor, err := strconv.Atoi(version.Minor)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Version{Major: major, Minor: minor}, nil
+}
