c895dd135ec47550f22e23df9640699380d18f0a
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2023-06-14 10:48:50 +0200

    LBSetContainsKeepalivedMaster is also true if there is no condition after 15 min to not block update

--- internal/helper/loadbalancerset.go
+++ internal/helper/loadbalancerset.go
@@ -131,11 +131,14 @@ func LoadBalancerSetIsReady(
 	return false, fmt.Errorf("active LoadBalancerSet not found")
 }
 
-// LBSetHasKeepalivedMaster returns true if the keepalived condition on set is ready for more than 2 min
-// or keepalived is not ready for more than 10 min (to make sure this do not block updates)
+// LBSetHasKeepalivedMaster returns true one of the following conditions are met:
+// - if the keepalived condition on set is ready for more than 2 min
+// - keepalived condition is not ready for more than 10 min (to make sure this does not block updates)
+// - no keepalived condition is in lbs but lbs is older than 15 min (to make sure this does not block updates)
 func LBSetHasKeepalivedMaster(set *yawolv1beta1.LoadBalancerSet) bool {
 	before2Minutes := metaV1.Time{Time: time.Now().Add(-2 * time.Minute)}
 	before10Minutes := metaV1.Time{Time: time.Now().Add(-10 * time.Minute)}
+	before15Minutes := metaV1.Time{Time: time.Now().Add(-15 * time.Minute)}
 
 	for _, condition := range set.Status.Conditions {
 		if condition.Type != HasKeepalivedMaster {
@@ -146,7 +149,7 @@ func LBSetHasKeepalivedMaster(set *yawolv1beta1.LoadBalancerSet) bool {
 		}
 		return condition.LastTransitionTime.Before(&before10Minutes)
 	}
-	return false
+	return set.CreationTimestamp.Before(&before15Minutes)
 }
 
 // Checks if LoadBalancerSets deriving from LoadBalancers are downscaled except for the LoadBalancerSet with the name of exceptionName
