91b4b5b4b3d8e140b58f82f97bec6a55434077ce
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2024-07-25 17:06:41 +0200

    Use loki-promtail pkg instead if promtail from docker

--- image/promtail.sh
+++ image/promtail.sh
@@ -2,7 +2,7 @@
 
 name=$RC_SVCNAME
 description="promtail"
-command="/usr/local/bin/promtail"
+command="/usr/bin/promtail"
 command_args="-config.file=/etc/promtail/promtail.yaml"
 command_user="promtail"
 command_background="yes"
